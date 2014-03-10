163b1be56a9a2b0eba58b04909ad70b2578eae69
Gerilyn Barsczewski <gbarscz1@umbc.edu>
2014-03-10 09:46:54 +0100

    Fixing some Ch 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,10 +43,12 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
-      #describe "followed by signout" do
-        #before { click_link "Sign out" }
-        #it { should have_link('Sign in') }
-      #end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+        it { should_not have_link('Profile',     href: user_path(user)) }
+        it { should_not have_link('Settings',    href: edit_user_path(user)) }
+      end
     end
 
     describe "for signed-in users" do
