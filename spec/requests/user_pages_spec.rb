d098d519393b83e637d2a87a5689c399b3020ad6
Gerilyn Barsczewski <gbarscz1@umbc.edu>
2014-03-10 11:07:28 +0100

    Chapter 10 Exercises Attempt

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -104,8 +104,8 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-	it { should have_welcome_message('Welcome')}#
-        #it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+	it { should have_welcome_message('Welcome')}
+        it { should have_welcome_message('Welcome')}
       
         describe "followed by signout" do
           before { click_link "Sign out" }
